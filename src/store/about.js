28c5448aefaec301c7b078fb5c0be0d5a4004778
shutopia <finoqkrtlgud@naver.com>
2021-07-04 21:41:49 +0900

    notfoundpage

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'shtopia',
+    email: 'finoqkrtlgud@naver.com',
+    blog: 'https://heropy.blog',
+    phone: '+82-010-3392-9553',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
